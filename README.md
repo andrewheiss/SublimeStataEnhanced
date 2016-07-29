76104da5c6ee75b968f85aab210a035c62733c9d
Benjamin Elbers <elbersb@gmail.com>
2016-07-29 10:22:33 +0200

    Merge version-specific commands into one

--- README.md
+++ README.md
@@ -7,13 +7,9 @@ This package basic support for Stata (11–14, Windows and OS X) and comes with:
 
 * Language definition for `.do` and `.ado` files 
 * Commands for sending individual lines or selections to Stata (available via the command palette and with keyboard shortcuts):
-	* (*Stata 13, 14, OS X only*) Send current line or selection to Stata: `super+enter`
-	* (*Stata 11, 12; OS X only*) Send current line or selection to Stata: `super+alt+enter`
-	* (*Stata 11–14; Windows only*) Send current line or selection to Stata: `ctrl+enter`
-* (*OS X only*) A build system for Stata files
-
-**NB**: If you use Stata < 13 on OS X, you'll probably want to reconfigure those keyboard shortcuts so that `super+enter` runs `stata-send-command-move-next-line.sublime-macro` instead of `stata-send-command-move-next-line13.sublime-macro`. Go to Preferences > Package Settings > Stata Enhanced > Keybindings – Default and copy the appropriate entry to your user keybindings (Preferences > Key Bindings – User).
-
+	* (*OS X*) Send current line or selection to Stata: `super+enter`
+	* (*Windows*) Send current line or selection to Stata: `ctrl+enter`
+* A build system for Stata files
 
 ## Background
 
@@ -43,10 +39,9 @@ By default, when sending code to Stata on OS X, Sublime Text will maintain focus
 ### Windows
 
 * (**All versions of Stata**) Set the full path of your Stata installation (with slashes reversed) in `Stata Enhanced (Windows).sublime-settings` (default is `"C:/Program Files (x86)/Stata13/StataSE-64.exe"`)
-* This package sends selected code to a temporary file and then opens that file in Stata via Finder. In order for it to work correctly on your system, you must ensure that `.do` files opened in Stata are run, not edited. Change this in Do-file Editor > Edit > Preferences > Advanced > Edit do-files opened from Windows instead of executing them (uncheck this)
+* This package sends selected code to a temporary file and then opens that file in Stata. In order for it to work correctly on your system, you must ensure that `.do` files opened in Stata are run, not edited. Change this in Do-file Editor > Edit > Preferences > Advanced > Edit do-files opened from Windows instead of executing them (uncheck this)
 
 
 ## Roadmap and wish list
 
 * Stata 13 for Windows [has support for Automation APIs](http://www.stata.com/automation/). It would be cool to use those instead of temporary files (like how the OS X version of the plugin uses temporary files for Stata 11–12, but the API for 13).
-* There is a setting for the Stata version, but it doesn't do anything yet. It would be nice to have run the appropriate command without having almost-duplicate `stata-send-command-move-next-line.sublime-macro` and `stata-send-command-move-next-line13.sublime-macro` files. However, I don't see a way to use setting variables in macro files. 
--- text_2_stata.py
+++ text_2_stata.py
@@ -91,15 +91,14 @@ def get_stata_version(stata_name):
         version = version.decode("utf-8").strip()
         return((int(version), "com.stata.stata{}".format(version)))
 
+
 class StataRunCompleteCommand(sublime_plugin.WindowCommand):
     """ Runs the complete do file in Stata. """
 
     def run(self):
         self.window.run_command('save')
         file_name = self.window.active_view().file_name()
-
         settings = sublime.load_settings(settingsfile)
-        version, stata_app_id = get_stata_version(settings.get('stata_name'))
 
         # Switch focus to Stata or not after sending a command depending on a setting
         if settings.get('switch_focus_to_stata'):
@@ -107,62 +106,43 @@ class StataRunCompleteCommand(sublime_plugin.WindowCommand):
         else:
             switch_focus = ""
 
-        cmd = """osascript<< END
-             tell application id "{}"
-                {}
-                DoCommandAsync "do \\\"{}\\\"" with addToReview
-             end tell
-             END""".format(stata_app_id, switch_focus, file_name)
+        if sublime.platform() == "osx":
+            version, stata_app_id = get_stata_version(settings.get('stata_name'))
 
-        os.system(cmd)
-
-
-class text_2_stata13Command(sublime_plugin.TextCommand):
-    """ Run selection or current line *directly* in Stata (for Stata 13) """
-
-    def run(self, edit):
-        settings = sublime.load_settings(settingsfile)
-
-        # Get the selection; if nothing is selected, get the current line
-        all_text = ""
-        sels = self.view.sel()
-        for sel in sels:
-            all_text = all_text + self.view.substr(sel)
-        if len(all_text) == 0:
-            all_text = self.view.substr(self.view.line(sel))
-        all_text = all_text + "\n"
-
-        # Get rid of inline comments
-        all_text = strip_inline_comments(all_text)
-        all_text = all_text.replace('\\', '\\\\\\').replace('"', '\\"'). \
-            replace('`', '\\`').replace('$', "\\$").strip()
-
-        # Switch focus to Stata or not after sending a command depending on a setting
-        if settings.get('switch_focus_to_stata'):
-            switch_focus = "activate"
-        else:
-            switch_focus = ""
-
-        version, stata_app_id = get_stata_version(settings.get('stata_name'))
-
-        # Stata only allows 8192 characters.
-        if len(all_text)<8192:
-            # Send the command to Stata with AppleScript
+        if sublime.platform() == "osx" and version >= 13:
             cmd = """osascript<< END
-             tell application id "{}"
-                {}
-                DoCommandAsync "{}" with addToReview
-             end tell
-             END""".format(stata_app_id, switch_focus, all_text)
-            print(cmd)
+                 tell application id "{}"
+                    {}
+                    DoCommandAsync "do \\\"{}\\\"" with addToReview
+                 end tell
+                 END""".format(stata_app_id, switch_focus, file_name)
             os.system(cmd)
-        else:
-            if sublime.ok_cancel_dialog("This selection is too long to run.\n\nWould you like to run the whole do file?", "Run complete file"):
-                self.view.window().run_command("stata_run_complete")
+        elif sublime.platform() == "osx" and version <= 12:
+            # Get ST version for returning focus to ST
+            if int(sublime.version()) > 3000:
+                st_name = "Sublime Text"
+            else:
+                st_name = "Sublime Text 2"
 
+            cmd = """osascript -e 'tell application "Finder" to open POSIX file "{0}"' -e 'tell application "{1}" to activate' &""".format(file_name, st_name)
+            os.system(cmd)
+        elif sublime.platform() == "windows":
+            filepath = os.path.dirname(file_name)
+            
+            vbs_cmd = """CreateObject("WScript.Shell").Run \"\""{0}\"" \""{1}\"\"", 4, false """.format(settings.get('stata_name'), file_name)
+
+            vbs_path = os.path.join(filepath, 'open_in_stata.vbs')
+            vbs_file = open(vbs_path, 'w')
+            vbs_file.write(vbs_cmd)
+            vbs_file.close()
+
+            cmd = "cscript.exe {0}".format("\"" + vbs_path + "\"")
+            os.system(cmd)
+            os.remove(vbs_path)
 
 class text_2_stataCommand(sublime_plugin.TextCommand):
-    """ Run selection or current line in Stata through a temporary file (for Stata < 13) """
+    """ Run selection or current line *directly* in Stata"""
+
     def run(self, edit):
         settings = sublime.load_settings(settingsfile)
 
@@ -178,18 +158,45 @@ class text_2_stataCommand(sublime_plugin.TextCommand):
         # Get rid of inline comments
         all_text = strip_inline_comments(all_text)
 
-        # Get the path to the current rile
-        filename = self.view.file_name()
-        filepath = os.path.dirname(filename)
-
-        # Write the selection to a temporary file in the current directory
-        dofile_path = os.path.join(filepath, 'sublime2stata.do')
-        this_file = open(dofile_path, 'w')
-        this_file.write(all_text)
-        this_file.close()
-
-        # Open the temporary do file in Stata
+        # Switch focus to Stata or not after sending a command depending on a setting
+        if settings.get('switch_focus_to_stata'):
+            switch_focus = "activate"
+        else:
+            switch_focus = ""
+    
         if sublime.platform() == "osx":
+            version, stata_app_id = get_stata_version(settings.get('stata_name'))
+
+        if sublime.platform() == "osx" and version >= 13:
+            all_text = all_text.replace('\\', '\\\\\\').replace('"', '\\"'). \
+                replace('`', '\\`').replace('$', "\\$").strip()
+
+            # Stata only allows 8192 characters.
+            if len(all_text)<8192:
+                # Send the command to Stata with AppleScript
+                cmd = """osascript<< END
+                 tell application id "{}"
+                    {}
+                    DoCommandAsync "{}" with addToReview
+                 end tell
+                 END""".format(stata_app_id, switch_focus, all_text)
+                print(cmd)
+                os.system(cmd)
+            else:
+                if sublime.ok_cancel_dialog("This selection is too long to run.\n\nWould you like to run the whole do file?", "Run complete file"):
+                    self.view.window().run_command("stata_run_complete")
+        
+        elif sublime.platform() == "osx" and version <= 12:
+            # Get the path to the current file
+            filename = self.view.file_name()
+            filepath = os.path.dirname(filename)
+
+            # Write the selection to a temporary file in the current directory
+            dofile_path = os.path.join(filepath, 'sublime2stata.do')
+            this_file = open(dofile_path, 'w')
+            this_file.write(all_text)
+            this_file.close()
+
             # Get ST version for returning focus to ST
             if int(sublime.version()) > 3000:
                 st_name = "Sublime Text"
@@ -198,7 +205,18 @@ class text_2_stataCommand(sublime_plugin.TextCommand):
 
             cmd = """osascript -e 'tell application "Finder" to open POSIX file "{0}"' -e 'tell application "{1}" to activate' &""".format(dofile_path, st_name)
             os.system(cmd)
+        
         elif sublime.platform() == "windows":
+            # Get the path to the current file
+            filename = self.view.file_name()
+            filepath = os.path.dirname(filename)
+
+            # Write the selection to a temporary file in the current directory
+            dofile_path = os.path.join(filepath, 'sublime2stata.do')
+            this_file = open(dofile_path, 'w')
+            this_file.write(all_text)
+            this_file.close()
+
             vbs_cmd = """CreateObject("WScript.Shell").Run \"\""{0}\"" \""{1}\"\"", 4, false """.format(settings.get('stata_name'), dofile_path)
 
             vbs_path = os.path.join(filepath, 'open_in_stata.vbs')
